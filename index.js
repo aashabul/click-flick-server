db9ee5dfa5e5bc13ccf12f7610320f9f7e012ca7
aashabul <aashabulimam@gmail.com>
2021-11-12 21:27:56 +0600

    server added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gmdfr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,10 +14,19 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('database connected')
+        const database = client.db('click_flick');
+        const productsCollection = database.collection('products');
+        console.log('hitting database');
+
+        // fetch products data from server
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products)
+        })
     }
     finally {
-
+        // await client.close();
     }
 }
 
